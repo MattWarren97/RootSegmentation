942683a4856913a50c89da96debe2c76b8c4c201
MattWarren97 <mfw1g15@soton.ac.uk>
2017-07-28 15:05:13 +0100

    tried to add in 'findChainedClusters' -- I think the initial tests show my method isn't working.

--- AA_plugins/Color_Segmenter.java
+++ AA_plugins/Color_Segmenter.java
@@ -31,6 +31,7 @@ public class Color_Segmenter extends SegmentationPlugin implements PlugInFilter 
 	static float areaDifferenceWeight = 1;
 	static float aspectRatioDifferenceWeight = 1;
 	static float maxCenterDistance = 5;
+	static int minClusterChainLength = 10;
 	
 	static {
 		lut = new int[256];
@@ -42,7 +43,7 @@ public class Color_Segmenter extends SegmentationPlugin implements PlugInFilter 
 
 	HashMap<Integer, ArrayList<Cluster>> sliceClusterMap;
 	HashMap<Integer, Integer> differenceCounts;
-	HashMap<Cluster, Cluster> connectedClusters;
+	HashMap<Integer, HashMap<Cluster, Cluster>> pairedClustersBySlice;
 
 
 
@@ -69,6 +70,7 @@ public class Color_Segmenter extends SegmentationPlugin implements PlugInFilter 
 	//multithreaded run version
 	public void run(ImageProcessor ip) {
 		this.sliceClusterMap = new HashMap<Integer, ArrayList<Cluster>>(); //does this need to be synchronised?
+		this.pairedClustersBySlice = new HashMap<Integer, HashMap<Cluster, Cluster>>();
 		ImageStack stack = this.image.getStack(); //this too..?
 		int processors = Runtime.getRuntime().availableProcessors();
 		float slicesPerThread = (float) stack.getSize() / (float) processors;
@@ -97,64 +99,92 @@ public class Color_Segmenter extends SegmentationPlugin implements PlugInFilter 
 		
 		//wait for the other threads to be finished...
 		System.out.println("Finished calculatign clusters for each image. Now trying to link them!");
-		printDifferenceCounts();
-	}
+		findConnectedClusters();
+		findChainedClusters();
+	}			
 			
-	
-
-
-	public void printDifferenceCounts() {
-		//{0=259633, 1=169307, 2=20573, 3=3660, 4=1021, 5=362, 6=101, 7=34, 8=14, 9=10, 10=1, 14=1} [more than 1 each]
-		//{0=257548, 1=131425, 2=15536, 3=2866, 4=824, 5=297, 6=86, 7=32, 8=8, 9=8, 10=1, 14=1} [only 1 each].
-
-		differenceCounts = new HashMap<Integer, Integer>();
-		connectedClusters = new HashMap<Cluster, Cluster>();
-		int nullCount = 0;
-		int nonNullCount = 0;
-		int duplicateFirstClusterCount = 0;
+		
+	public void findConnectedClusters() {
+		//500x500x300 - {0=259633, 1=169307, 2=20573, 3=3660, 4=1021, 5=362, 6=101, 7=34, 8=14, 9=10, 10=1, 14=1} [more than 1 each]
+		//500x500x300 - {0=257548, 1=131425, 2=15536, 3=2866, 4=824, 5=297, 6=86, 7=32, 8=8, 9=8, 10=1, 14=1} [only 1 each].
+		//A_cropped - {0=1891759, 1=395663, 2=19213, 3=1285, 4=303, 5=153, 6=130, 7=283, 8=701, 9=892, 10=573, 11=239, 12=66, 13=15, 14=1, 15=13}
+		HashMap<Cluster, Cluster> connectedClusters;
 		for (int i = 1; i <= this.image.getStackSize() - 1; i++) {
-			ArrayList<Cluster> iClusters = sliceClusterMap.get(i);
-			ArrayList<Cluster> ippClusters = sliceClusterMap.get(i+1);
-			for (Cluster c1: iClusters) {
+			
+			connectedClusters = new HashMap<Cluster, Cluster>();
+			ArrayList<Cluster> clusters1 = sliceClusterMap.get(i);
+			ArrayList<Cluster> clusters2 = sliceClusterMap.get(i+1);
+			for (Cluster c1: clusters1) {
 				float minDifference = -1;
-				for (Cluster c2 : ippClusters) {
-					if (c1 == null) {
-						System.out.println("c1 is null!");
-					}
-					if (c2 == null) {
-						System.out.println("c2 is null!");
-					}
-					//System.out.println(c1.center + " / " + c1.area + " / " + c1.aspectRatio + " / " + c1.colour);
+				Cluster bestCluster = null;
+				for (Cluster c2 : clusters2) {
 					Float difference = compareClusters(c1, c2);
 					if (difference == null) {
-						nullCount++;
 						continue;
 					}
-					nonNullCount++;
-					//automatically round down.
-					Integer floorDifference = (int) ((float) difference);
 					
 					if (difference < minDifference || minDifference == -1) {
-						
-						if (differenceCounts.keySet().contains(floorDifference)) {
-							differenceCounts.put(floorDifference, differenceCounts.get(floorDifference) + 1);
-						}
-						else {
-							differenceCounts.put(floorDifference, 1);
-						}
 						minDifference = difference;
+						bestCluster = c2;
 					}
-					else {
-						duplicateFirstClusterCount++;
-					}
+				}
+				if (minDifference != -1) {
+					connectedClusters.put(c1, bestCluster);
 				}
 			}
 			System.out.println("Completed difference mapping for slice " + i);
+			pairedClustersBySlice.put(i, connectedClusters);
 		}
+	}
+	
+	//minimum length of chains found will be 2 bc it is known they are connected to at least one...
+	
+	//problem is that A-B-C-D-E-F_G... will also get picked up as B-C_D_E_F_G.
+	//I could remove B-C_D_E_F_G once I've found the chain A-B_C_D_E_F_G,
+	//but it is entirely possible to have: A-B-C-D-E-F-G AND A2-B-C-D-E-F-G-... 
+	//so I guess B and it's consequent chain should be stored as a known chain that can be linked to again...
+	//(same thing will need to be done for C-D-E-F_G... and D-E-F-G... etc.)
+	//that's a pain!
+	
+	//results:
+	//Cluster lengths: {2=95660, 3=64261, 4=45500, 5=33396, 6=24993, 7=19255, 8=14995, 9=11668, 10=9163, 11=7350, 12=5746, 13=4562, 14=3582, 15=2839, 16=2278, 17=1818, 18=1454, 19=1166, 20=884, 21=741, 22=584, 23=472, 24=396, 25=318, 26=269, 27=225, 28=186, 29=159, 30=129, 31=94, 32=81, 33=73, 34=70, 35=61, 36=48, 37=47, 38=35, 39=32, 40=34, 41=29, 42=27, 43=27, 44=22, 45=19, 46=20, 47=22, 48=19, 49=15, 50=12, 51=9, 52=8, 53=8, 54=7, 55=5, 56=5, 57=4, 58=4, 59=4, 60=3, 61=3, 62=4, 63=3, 64=3, 65=3, 66=3, 67=3, 68=4, 69=4, 70=4, 71=6, 72=6, 73=3, 74=3, 75=2, 76=1, 77=1, 78=1, 79=1, 80=1, 81=1, 82=2, 83=2, 84=3, 85=2, 86=2, 87=1, 88=1, 89=1, 90=1, 91=1, 92=1, 93=1, 94=1, 95=3, 96=2, 97=1, 98=1, 99=1, 100=1, 101=2, 102=1, 103=1, 104=1, 105=1, 106=1, 107=2, 108=2, 109=2, 110=2, 111=1, 112=1, 113=1, 114=1, 115=1, 116=1, 117=1, 118=1, 119=1, 120=1, 121=1, 122=1, 123=1, 124=1, 125=1, 126=1, 127=1, 128=1, 129=1, 130=1, 131=1, 132=1, 133=1, 134=2, 135=3, 136=2, 137=3, 138=3, 139=2, 140=1, 141=1, 142=2}
 
-		System.out.println("Finally! " + differenceCounts);
-		System.out.println("NullCount: " + nullCount + ", nonNullCount: " + nonNullCount + ", duplicateFirstClusterCount: " + duplicateFirstClusterCount);
-
+	//These aren't consistent with what I expected... How can there be 2 at length 142, but only 1 at length 141?
+	//need to fix this I suppose.
+	//Then I can get to visualising the results.
+	public void findChainedClusters() {
+		HashMap<Integer, Integer> clusterLengths = new HashMap<Integer, Integer>();
+		int stackSize = this.image.getStackSize();
+		for (int i = 1; i <= stackSize - minClusterChainLength; i++) {
+			HashMap<Cluster, Cluster> connectedClusters = pairedClustersBySlice.get(i);
+			
+			for (Cluster firstKey: connectedClusters.keySet()) {
+				Cluster key = firstKey;
+				Cluster next;
+				int length = 0;
+				do {
+					if (key.getSliceNumber() == stackSize) {
+						length++;
+						break;
+					}
+					next = pairedClustersBySlice.get(key.getSliceNumber()).get(key);
+					key = next;
+					length++;
+				}
+				while(key != null);
+				if (length > minClusterChainLength) {
+					System.out.println("Cluster " + firstKey + " had a chainLength of " + length);
+				}
+				if (clusterLengths.containsKey(length)) {
+					clusterLengths.put(length, clusterLengths.get(length) + 1);
+				}
+				else {
+					clusterLengths.put(length, 1);
+				}
+			}
+		}
+		
+		System.out.println("Cluster lengths: " + clusterLengths);
 	}
 
 
@@ -190,21 +220,23 @@ class ObjectFinder implements Runnable {
 	ArrayList<Point> toBeProcessed;
 	ArrayList<Point> sameClusterToBeProcessed;
 	Point[][] points;
+	int sliceNumber;
 		
 	public ObjectFinder(int start, int end, ImageStack stack, Color_Segmenter cs) {
 		this.start = start;
 		this.end = end;
 		this.stack = stack;
 		this.cs = cs;
+
 	}
 	public void run() {
 		System.out.println("Thread between " + start + "-" + end + " has begun.");
-		for (int i = start; i <= end; i++) {
-			System.out.println("Run on Slice: " + i);
-			ImageProcessor nextSlice = stack.getProcessor(i);
+		for (sliceNumber = start; sliceNumber <= end; sliceNumber++) {
+			System.out.println("Run on Slice: " + sliceNumber);
+			ImageProcessor nextSlice = stack.getProcessor(sliceNumber);
 			convertTo4Bit(nextSlice);
 			connectivityAnalysis(nextSlice);
-			cs.sliceClusterMap.put(i, largeClusters);
+			cs.sliceClusterMap.put(sliceNumber, largeClusters);
 		}
 	}
 	
@@ -243,7 +275,7 @@ class ObjectFinder implements Runnable {
 			}
 			else {
 				currentPoint = toBeProcessed.get(0);
-				this.clusters.add(new Cluster(currentPoint));
+				this.clusters.add(new Cluster(currentPoint, sliceNumber));
 			}
 			//System.out.println("New point is " + currentPoint);
 			//System.out.println("Length of toBeProcessed, sameClusterToBeProcessed is " + toBeProcessed.size() + "," + sameClusterToBeProcessed.size());
@@ -353,13 +385,15 @@ class Cluster {
 	public int area;
 	public float aspectRatio;
 	public float[] center;
+	public int z;
 
-	public Cluster(Point p) {
+	public Cluster(Point p, int z) {
 		this.points = new ArrayList<Point>();
 		this.neighbours = new ArrayList<Cluster>();
 		this.futureNeighbours = new ArrayList<Point>();
 		this.addPoint(p);
 		this.colour = p.value;
+		this.z = z;
 	}
 
 	public void addPoint(Point p) {
@@ -450,5 +484,9 @@ class Cluster {
 		this.area = getArea();
 		this.aspectRatio = getAspectRatio();
 		this.center = getCenter();
+	}
+	
+	public int getSliceNumber() {
+		return this.z;
 	}
 }
