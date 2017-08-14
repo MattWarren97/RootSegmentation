1c5637f09bc952610cfab397ecf1fe17051a93b9
MattWarren97 <mfw1g15@soton.ac.uk>
2017-08-14 15:05:47 +0100

    fixed compilePlugin.sh to find the jar

--- compilePlugin.sh
+++ compilePlugin.sh
@@ -1,1 +1,1 @@
-javac $1 -d ~/Downloads/Fiji.app/plugins -cp ~/Downloads/Fiji.app/jars/ij-1.51n.jar:/home/matt/Documents/#Work/RootSegmentation/
+javac $1 -d ~/Downloads/Fiji.app/plugins -cp ~/Downloads/Fiji.app/jars/ij-1.51n.jar:/home/matt/Documents/#Work/RootSegmentation/jars/commons-collections4-4.1.jar:/home/matt/Documents/#Work/RootSegmentation/
